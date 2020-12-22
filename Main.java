9d126335a4131323cf7d938062c84e800e31d5c6
edytaxkowalczyk <74655302+edytaxkowalczyk@users.noreply.github.com>
2020-12-22 12:41:46 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,8 @@
-import java.lang.*;
-
 public class Main{
 	public static void main(String[] args){
+		System.out.println("Kontener: Wersja 1");
 		while(true){
-			try {
-				 Thread.sleep(5000);
-			}
-			catch (Exception e)
-			{
-				e.printStackTrace();
-			}
-      System.out.println("Kontener: Wersja 1");
+		
 		}
 	}
 }
